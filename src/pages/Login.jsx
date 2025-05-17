13ffded1e837062991910cbc554a044ca1dac7d5
FernandoEspidio <fer618@hotmail.com>
2025-05-17 03:44:54 -0600

    non code corrections

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,7 @@
 import * as React from 'react';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage } from '@toolpad/core/SignInPage';
-import { useTheme } from '@mui/material/styles';
+
 import { Box, Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
-// // preview-start
-// const providers = [{ id: 'credentials', name: 'Email and Password' }];
-// // preview-end
-
-// const signIn = async (provider, formData) => {
-//   const promise = new Promise((resolve) => {
-//     setTimeout(() => {
-//       alert(
-//         `Signing in with "${provider.name}" and credentials: ${formData.get('email')}, ${formData.get('password')}`,
-//       );
-//       resolve();
-//     }, 300);
-//   });
-//   return promise;
-// };
-
-// export default function CredentialsSignInPage() {
-//   const theme = useTheme();
-//   return (
-//     // preview-start
-//     <AppProvider theme={theme}>
-//       <SignInPage
-//         signIn={signIn}
-//         providers={providers}
-//         slotProps={{ emailField: { autoFocus: false } }}
-//       />
-//     </AppProvider>
-//     // preview-end
-//   );
-// }
 
 const Login = ({login}) => {
   const navigate = useNavigate();
